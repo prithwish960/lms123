a9865ea837d7f6e7adb0c076bfbfe0be57e9c066
prithwish960 <60845878+prithwish960@users.noreply.github.com>
2022-07-16 17:32:31 +0530

    Update settings.py

--- LMS/settings.py
+++ LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1fwh%v)^g*4ao7kqsbdmlqceq&fx+j!p%=x3^f+51gu^+v*m=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lms123lms.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['https://github.com/prithwish960/lms123/']
 
 
 # Application definition
